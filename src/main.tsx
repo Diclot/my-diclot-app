bb583f8a647409d65ce6d3110e01a1d4a662f235
unknown <dicloselemani@gmail.com>
2023-04-12 16:55:45 +0300

    i added a files

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import "./index.css";
+
+
+const App =()=>{
+
+    const User = {
+        name: " diclot",
+        age: 22,
+        email: "diclot@gmail.com",
+    };
+    return(
+        <>
+< USerCard user = {user}/>
+
+
+        </>
+    );
+
+}   ;
+const 
+
+
+
+ const Root =()     ReactDOM.createRoot(Document.getElementById("root"));
+Root.Rende(
+
+    <App/>
+)
