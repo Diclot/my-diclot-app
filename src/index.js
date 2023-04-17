766d83c1bf64d6ccad10912c95c8c2ff05d41a37
unknown <dicloselemani@gmail.com>
2023-04-17 16:52:53 +0300

    i add a form

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import Books from './books';
 import "./index.css"
 
+
+const App =()=>{
+    const [emailValue, setEmailValue]= useState("");
+    const [passwordValue, setPassword]= useState("");
+
+const onEmailvalueChangeHandler = (e)=>{
+    setEmailValue(e.target.value);
+
+
+};
+const onpasswordvalueChangeHandler =(e)=>{
+    setPasswordValue(e.target.value)
+}
+
+    return(
+        <>
+        <h1>React Form</h1>
+        <Form emailValue={emailValue} 
+         passwordValue={passwordValue} 
+
+         onEmailvalueChangeHandler={onEmailvalueChangeHandler}
+         onpasswordvalueChangeHandler={onpasswordvalueChangeHandler}
+         />
+
+        
+        </>
+    );
+}
+
+const Form=(prop)=>{
+    return(
+        <form>
+            <label htmlFor="email">
+                Email:
+                <input type="email" value={prop.emailValue} onChange={prop. onEmailvalueChangeHandler}/>
+                <br>
+                </br>
+            </label> 
+
+        <label htmlFor="password">
+            Password:
+            <input type="password"  value={prop.passwordValue} onChange={prop.onPasswordvalueChangeHandler}/>
+                <br>
+                </br>
+        </label>
+        < button type=" submit">
+            sign in
+        </button>
+
+        </form>
+    )
+}
+
+
+   
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Books/>
+    <App/>
 
 );
 
