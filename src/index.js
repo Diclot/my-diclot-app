d203c600ecdbb435aa82daf74ffbdf4d7570cf71
unknown <dicloselemani@gmail.com>
2023-04-27 15:37:03 +0300

    i have compited

--- src/index.js
+++ src/index.js
@@ -1,67 +1,25 @@
-import React ,{useState} from 'react';
-import ReactDOM from 'react-dom/client';
-import "./index.css"
 
 
-const App =()=>{
-    const [emailValue, setEmailValue]= useState("");
-    const [passwordValue, setPassword]= useState("");
-
-const onEmailvalueChangeHandler = (e)=>{
-    setEmailValue(e.target.value);
-
-
-};
-const onpasswordvalueChangeHandler =(e)=>{
-    setPasswordValue(e.target.value)
-}
-
-    return(
-        <>
-        <h1>React Form</h1>
-        <Form emailValue={emailValue} 
-         passwordValue={passwordValue} 
-
-         onEmailvalueChangeHandler={onEmailvalueChangeHandler}
-         onpasswordvalueChangeHandler={onpasswordvalueChangeHandler}
-         />
-
-        
-        </>
-    );
-}
-
-const Form=(prop)=>{
-    return(
-        <form>
-            <label htmlFor="email">
-                Email:
-                <input type="email" value={prop.emailValue} onChange={prop. onEmailvalueChangeHandler}/>
-                <br>
-                </br>
-            </label> 
-
-        <label htmlFor="password">
-            Password:
-            <input type="password"  value={prop.passwordValue} onChange={prop.onPasswordvalueChangeHandler}/>
-                <br>
-                </br>
-        </label>
-        < button type=" submit">
-            sign in
-        </button>
-
-        </form>
-    )
-}
+var obj = {FirstName:"john",lastName:"Doe",age:30};
+let FirstName= "john"
+    lastName= "Doe"
+    
+console.log(obj)
 
 
-   
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
 
-);
+
+
+
+
+
+
+
+
+// 
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
